4ea3f167168c3562ec7112cff726495c5ec82619
smskbuilds <smskbuilds@gmail.com>
2024-10-15 10:24:14 -0700

    Added example stock API response

--- index.js
+++ index.js
@@ -8,11 +8,42 @@ const openai = new OpenAI({
 const messages = [
     {
         role: 'system',
-        content: 'You are Benjamin Franklin.',
+        content:
+            'You are a trading guru. Given data on share prices over the past 3 days, write a report of no more than 150 words describing the stocks performance and recommending whether to buy, hold or sell.',
     },
     {
         role: 'user',
-        content: 'Who invented the television?',
+        content: `{
+  "adjusted": true,
+  "next_url": "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/1578114000000/2020-01-10?cursor=bGltaXQ9MiZzb3J0PWFzYw",
+  "queryCount": 2,
+  "request_id": "6a7e466379af0a71039d60cc78e72282",
+  "results": [
+    {
+      "c": 75.0875,
+      "h": 75.15,
+      "l": 73.7975,
+      "n": 1,
+      "o": 74.06,
+      "t": 1577941200000,
+      "v": 135647456,
+      "vw": 74.6099
+    },
+    {
+      "c": 74.3575,
+      "h": 75.145,
+      "l": 74.125,
+      "n": 1,
+      "o": 74.2875,
+      "t": 1578027600000,
+      "v": 146535512,
+      "vw": 74.7026
+    }
+  ],
+  "resultsCount": 2,
+  "status": "OK",
+  "ticker": "AAPL"
+}`,
     },
 ];
 
@@ -21,4 +52,4 @@ const response = await openai.chat.completions.create({
     messages: messages,
 });
 
-console.log(response);
+console.log(response.choices[0].message.content);
